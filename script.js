bd69e56aa3f70398bd1f596ebee8a515f261c684
Snox <104462418+SnoxOnGitHub@users.noreply.github.com>
2022-05-06 19:49:32 +0200

    Added data structure

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const linemenu = document.getElementsByClassName('linemenu')[0]
 const menu = document.getElementsByClassName('menu')[0]
-const menuelements = document.getElementsByClassName('menu_elements')[0]
 
 linemenu.addEventListener('click', () => {
     menu.classList.toggle('active')
     linemenu.classList.toggle('active')
-    menuelements.classList.toggle('active')
 })
