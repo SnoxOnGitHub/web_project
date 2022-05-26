49d9bbb1515f1b12ebce2861282d9d9febb2891e
Snox <104462418+SnoxOnGitHub@users.noreply.github.com>
2022-05-26 20:20:37 +0200

    dynamic upgrade

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const linemenu = document.getElementsByClassName('linemenu')[0]
-const menu = document.getElementsByClassName('menu')[0]
+const page_url = "https://snoxongithub.github.io/web_project/"
 
-linemenu.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    linemenu.classList.toggle('active')
+function urls(){
+    document.getElementById('favicon').href = page_url + "images/Golden_Circles.svg"
+
+    document.getElementsByClassName('about')[0].href = page_url + "about/"
+    document.getElementsByClassName('crowdfunding')[0].href = page_url + "crowdfunding/"
+    document.getElementsByClassName('contact')[0].href = page_url + "contact/"
+    document.getElementsByClassName('faq')[0].href = page_url + "faq/"
+
+    document.getElementsByClassName('about')[1].href = page_url + "about/"
+    document.getElementsByClassName('crowdfunding')[1].href = page_url + "crowdfunding/"
+    document.getElementsByClassName('contact')[1].href = page_url + "contact/"
+    document.getElementsByClassName('faq')[1].href = page_url + "faq/"
+}
+
+window.onload = function() {
+    urls()
+}
+
+document.getElementsByClassName('linemenu')[0].addEventListener('click', () => {
+    document.getElementsByClassName('menu')[0].classList.toggle('active')
+    document.getElementsByClassName('linemenu')[0].classList.toggle('active')
 })
