31ec82e19634f8c28a6568afc67fcb238803841a
Snox <104462418+SnoxOnGitHub@users.noreply.github.com>
2022-05-27 13:37:31 +0200

    Better SVG update

--- crowdfunding/script.js
+++ crowdfunding/script.js
@@ -1,17 +1,20 @@
 const page_url = "https://snoxongithub.github.io/web_project/"
 
 function urls(){
+
     document.getElementById('favicon').href = page_url + "images/Golden_Circles.svg"
 
-    document.getElementsByClassName('about')[0].href = page_url + "about/"
-    document.getElementsByClassName('crowdfunding')[0].href = page_url + "crowdfunding/"
-    document.getElementsByClassName('contact')[0].href = page_url + "contact/"
-    document.getElementsByClassName('faq')[0].href = page_url + "faq/"
+    document.getElementById('logo_href').href = page_url
 
-    document.getElementsByClassName('about')[1].href = page_url + "about/"
-    document.getElementsByClassName('crowdfunding')[1].href = page_url + "crowdfunding/"
-    document.getElementsByClassName('contact')[1].href = page_url + "contact/"
-    document.getElementsByClassName('faq')[1].href = page_url + "faq/"
+    document.getElementsByClassName('about')[0].href = page_url + "about"
+    document.getElementsByClassName('crowdfunding')[0].href = page_url + "crowdfunding"
+    document.getElementsByClassName('contact')[0].href = page_url + "contact"
+    document.getElementsByClassName('faq')[0].href = page_url + "faq"
+
+    document.getElementsByClassName('about')[1].href = page_url + "about"
+    document.getElementsByClassName('crowdfunding')[1].href = page_url + "crowdfunding"
+    document.getElementsByClassName('contact')[1].href = page_url + "contact"
+    document.getElementsByClassName('faq')[1].href = page_url + "faq"
 }
 
 window.onload = function() {
